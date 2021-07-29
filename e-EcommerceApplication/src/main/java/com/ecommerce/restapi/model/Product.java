59761babfb6ef1000d1fd15df58773d9624fff33
nagesh konatham <nagu.kontham@gmail.com>
2021-07-29 13:18:23 +0530

    latest changes

--- e-EcommerceApplication/src/main/java/com/ecommerce/restapi/model/Product.java
+++ e-EcommerceApplication/src/main/java/com/ecommerce/restapi/model/Product.java
@@ -53,32 +53,21 @@ public class Product
     {
     	
     }
-	
 	public Long getProductId() {
 		return productId;
 	}
-
-
 	public void setProductId(Long productId) {
 		this.productId = productId;
 	}
-
-
 	public String getProductName() {
 		return productName;
 	}
-
-
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
-
-
 	public String getProductDescription() {
 		return productDescription;
 	}
-
-
 	public void setProductDescription(String productDescription) {
 		this.productDescription = productDescription;
 	}
